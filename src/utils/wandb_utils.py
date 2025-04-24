b0da1a8a1ca64b549917fa55483c86ee72f4f9dd
Alexey Shaturnyy <71039746+allexeyj@users.noreply.github.com>
2025-04-24 14:29:30 +0300

    Update wandb_utils.py

--- src/utils/wandb_utils.py
+++ src/utils/wandb_utils.py
@@ -1,8 +1,10 @@
 import os
 import wandb
+from omegaconf import OmegaConf
+
 
 def init_wandb(cfg):
     if cfg.wandb.api_key:
         os.environ["WANDB_API_KEY"] = cfg.wandb.api_key
     wandb.login()
-    wandb.init(project=cfg.wandb.project, config=cfg)
+    wandb.init(project=cfg.wandb.project, config=OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True))
