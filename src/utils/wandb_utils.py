4b89e19e10a6d4ace39965b76f589e938ae11861
Alexey Shaturnyy <71039746+allexeyj@users.noreply.github.com>
2025-04-24 12:46:24 +0300

    Update wandb_utils.py

--- src/utils/wandb_utils.py
+++ src/utils/wandb_utils.py
@@ -2,6 +2,7 @@ import os
 import wandb
 
 def init_wandb(cfg):
-    os.environ["WANDB_API_KEY"] = cfg.wandb.api_key
+    if cfg.wandb.api_key:
+        os.environ["WANDB_API_KEY"] = cfg.wandb.api_key
     wandb.login()
     wandb.init(project=cfg.wandb.project, config=cfg)
